607fc8caed81923c71061f910607792921f0ab82
ssyevuo <shakirasyevuo@gmail.com>
2024-11-27 23:56:51 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.querySelector('main#main');
+if (main) {
+    main.remove();
+}
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'Sean is the champion';
